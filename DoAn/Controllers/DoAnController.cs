83a7a73f0d117c31d5ce0382cb0636e1b2680cf5
trinhtran2709 <trinh0898792559@gmail.com>
2025-10-07 11:12:04 +0700

    Thêm toàn bộ project DoAn

---
+++ DoAn/Controllers/DoAnController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DoAn.Controllers
+{
+    public class DoAnController : Controller
+    {
+        // GET: DoAn
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult HeaderPartial()
+        {
+            return PartialView();
+        }
+        public ActionResult NavPartial()
+        {
+            return PartialView();
+        }
+        public ActionResult SliderPartial()
+        {
+            return PartialView();
+        }
+        public ActionResult FooterPartial()
+        {
+            return PartialView();
+        }
+        public ActionResult HaisanbanchayPartial()
+        {
+            return PartialView();
+        }
+
+
+
+    }
+}
